934705ce0966390e54e34313e1ecf5736a4bd532
Skipper Harris <skipdawg3000@gmail.com>
2022-01-18 14:33:54 -0800

    end of sprint commit

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// TODO
+var App = () => {
+  <div>
+    <GroceryList groceryItems={['milk', 'bread']}/>
+  </div>
+};
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+    fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+    <li style={style} onMouseEnter={this.onItemHover.bind(this)} onMouseLeave={this.onItemHover.bind(this)}>{this.props.groceryItems}</li>
+    );
+	}
+}
+
+var GroceryList = (props) => {
+  <ul>
+    {props.groceryItems.map(items =>
+      <GroceryListItem groceryItems={items}/>
+    )}
+  </ul>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
